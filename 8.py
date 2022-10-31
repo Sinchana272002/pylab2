3a1b68d8c6009a2d6e89e7d4e6e303cac99b7c4b
Sinchana272002 <117058117+Sinchana272002@users.noreply.github.com>
2022-10-31 09:44:53 +0530

    Add files via upload

---
+++ 8.py
@@ -0,0 +1,8 @@
+n=float(input('enter the number'))
+if n >= 0:
+  if n == 0:
+      print('it is zero')
+  else:
+      print('positive number')
+else:
+      print('negative number')
---
+++ 9.py
@@ -0,0 +1,10 @@
+a=float(input('enter the value of a'))
+b=float(input('enter the value of b'))
+c=float(input('enter the value of c'))
+if (a > b) and (b > c):
+    print('a is greatest')
+elif (b > a) and (a > c ):
+    print('b is greatest')
+else:
+      print('c is greatest')
+
